8f3c329c56082001cb80e321001c4fb698381944
Hihiz <public@mail.ru>
2022-10-22 02:14:06 +0300

    ConsoleApp + EF(Entity Framework Core)

--- ConsoleApp_EF/Program.cs
+++ ConsoleApp_EF/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace ConsoleApp_EF
+﻿using ConsoleApp_EF.Models;
+
+namespace ConsoleApp_EF
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 23 };
+                User user2 = new User { Name = "Alex", Age = 20 };
+                User user3 = new User { Name = "John", Age = 25 };
+
+                db.Add(user1);
+                db.Add(user2);
+                db.Add(user3);
+                db.SaveChanges();
+
+                var users = db.Users.ToList();
+
+                Console.WriteLine("Данные:");
+                foreach (User u in users)
+                    Console.WriteLine($"{u.Id}. {u.Name} - {u.Age}");
+            }
         }
     }
 }
